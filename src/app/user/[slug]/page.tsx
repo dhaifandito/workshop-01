b09cfd4cdc7fa9a2ef6ffb61e1ed12cff306111f
dhaifandito <dhaifan.da@gamail.com>
2024-02-24 17:43:57 +0700

    fix user

--- src/app/user/[slug]/page.tsx
+++ src/app/user/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-export default function User() {
+export default function Page({ params }: { params: { slug: string } }) {
   return <div className="font-robotomono flex justify-center items-center m-10 flex-col">
-  <h1 className="text-3xl my-7">User</h1>
+  <h1 className="text-3xl my-7">User {params.slug}</h1>
   </div>
 }
--- src/lib/components/Header.tsx
+++ src/lib/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className="flex justify-around items-center bg-slate-200 p-4">
       <Link href="/">Home</Link>
-      <Link href="/user">User</Link>
+      <Link href="/user/1">User</Link>
       <Link href="/login">Login</Link>
     </div>
   );
